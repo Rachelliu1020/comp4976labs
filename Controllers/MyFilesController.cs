0b39795a34e70054b6748e0359e7340d6652d12e
rachelliu1020 <rachelliu1020@gmail.com>
2017-01-24 13:53:42 -0800

    first commit

---
+++ Controllers/MyFilesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lab02.Controllers
+{
+    public class MyFilesController : Controller
+    {
+        // GET: MyFiles
+        public ActionResult Index()
+        {
+            string[] files = Directory.GetFiles(Server.MapPath("~/TextFiles"));
+            return View(files);
+        }
+
+        // GET: MyFiles
+        public ActionResult MyContent(string id)
+        {
+            string filename = id + ".txt";
+            string[] filePaths = Directory.GetFiles(Server.MapPath("~/TextFiles")).Where(ff => Path.GetFileName(ff) == filename).ToArray();
+         
+            
+            return View(filePaths);
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(lab02.Startup))]
+namespace lab02
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
